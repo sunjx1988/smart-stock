cd2ab46bc1df764e260aca3b12a37959fe150b46
sunjx <sunjx1988@163.com>
2019-03-10 17:12:17 +0800

    添加实体类 -> 股票

---
+++ src/main/java/smart/stock/entity/Stock.java
@@ -0,0 +1,45 @@
+package smart.stock.entity;
+
+import java.util.Date;
+
+public class Stock {
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ src/main/java/smart/stock/mapper/StockMapper.java
@@ -0,0 +1,30 @@
+package smart.stock.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import smart.stock.entity.Stock;
+import smart.stock.entity.StockExample;
+
+public interface StockMapper {
+    long countByExample(StockExample example);
+
+    int deleteByExample(StockExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Stock record);
+
+    int insertSelective(Stock record);
+
+    List<Stock> selectByExample(StockExample example);
+
+    Stock selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Stock record, @Param("example") StockExample example);
+
+    int updateByExample(@Param("record") Stock record, @Param("example") StockExample example);
+
+    int updateByPrimaryKeySelective(Stock record);
+
+    int updateByPrimaryKey(Stock record);
+}
